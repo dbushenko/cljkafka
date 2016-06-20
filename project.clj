5cfe945f8e0e2a638e3823198284394105a91212
dbushenko <d.bushenko@gmail.com>
2016-06-20 18:12:36 +0300

    fixed project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject cljkafka "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cljkafka "0.2.0-SNAPSHOT"
+  :description "Simple kafka wrapper"
+  :url "https://github.com/dbushenko/cljkafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [ [org.clojure/clojure "1.8.0"]
                                         [org.apache.kafka/kafka-clients "0.10.0.0"]
-                                        [inspector "0.1.1-SNAPSHOT"]
                                         [clojurewerkz/propertied "1.2.0"]
                                         ])
